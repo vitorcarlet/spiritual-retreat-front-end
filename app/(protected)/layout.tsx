c93f1b59f625bb9b02aa69cda6f1b102292d7b24
vitorcarlet <vitor.carlet27@gmail.com>
2025-07-14 21:03:39 -0300

    creating a layout for the protected routes.

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,3 +1,7 @@
+import SideMenu from "@/src/components/navigation/Menu";
+import TopBar from "@/src/components/navigation/TopBar";
+import { Box } from "@mui/material";
+
 export const metadata = {
   title: "Protected Routes",
 };
@@ -7,5 +11,18 @@ export default async function ProtectedLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  <Box >
+    <SideMenu />
+    <Box>
+
+        <TopBar />
+      
+      <Box  sx={{
+            flexGrow: 1,
+            p: 2,
+            transition: "margin 0.3s ease",
+          }}>{children}</Box>
+    </Box>
+  </Box>
+  return ;
 }
